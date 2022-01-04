864fdb0b4efacd60a92c87166eec39471e4eba02
José García <gran.pepe@gmail.com>
2022-01-04 18:21:41 +0100

    Entendiendo minilibx

--- fdf.h
+++ fdf.h
@@ -1,4 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-//#include "./mlx_linux/mlx.h"
-#include <mlx.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 16:42:38 by josgarci          #+#    #+#             */
+/*   Updated: 2022/01/04 17:02:11 by josgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+//# include "./mlx_linux/mlx.h"
+# include <mlx.h>
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+#endif 
