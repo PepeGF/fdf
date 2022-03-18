0883896dd12e31eeaf2ae56dbf4ea995ca2c1545
José García <gran.pepe@gmail.com>
2022-03-18 19:28:27 +0100

    Empezando funciones minilibx

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 16:42:38 by josgarci          #+#    #+#             */
-/*   Updated: 2022/03/16 22:23:34 by josgarci         ###   ########.fr       */
+/*   Updated: 2022/03/18 18:22:41 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,14 @@
 /* ni idea de para que usar esta estructura, creo que es para algo de minilib*/
 typedef struct s_data
 {
+	void	*mlx_ptr;
+	void	*mlx_win;
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_lenght;
 	int		endian;
+	int		offset;
 	int		map_x;
 	int		map_y;
 }	t_data;
@@ -74,7 +77,7 @@ typedef struct s_vars
 /* Manage map functions*/
 void	ft_map_read(t_list **raw_map, char *file);
 void	ft_map_raw_free(t_list *raw_map);
-void	ft_map_int_array(t_list *raw_map, t_point **points, t_coord map_size);
+void	ft_map_int_array(t_list *raw_map, t_point **points);
 int		ft_map_length(char *line);
 void	ft_map_free_array(t_point **points, t_coord map_size);
 void	ft_map_create_array(t_point ***points, t_coord map_size);
