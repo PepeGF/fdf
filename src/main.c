a2bfbff902eca4608d81f17531a1ed2653e5f191
Pepe <pepe@Pepes-Mac.local>
2022-03-26 13:12:41 +0100

    Conversión de puntos buena. Ajustar tamaño

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #define MLX_ERROR 1
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
+
 void	leakss()
 {
 	system ("leaks fdf");
@@ -20,10 +21,12 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 int	key_hook(int keycode, t_data *data)
 {
 	(void)data;
-	if (keycode == 53)
+	if (keycode == 53 || keycode == 12)
 	{
+		mlx_clear_window(data->mlx_ptr, data->mlx_win);
+		mlx_destroy_window(data->mlx_ptr, data->mlx_win);
 		write(1, "Bye bye!\n", 9);
-		exit(0);
+		return (0);
 	}
 	else
 		return (0);
@@ -31,6 +34,9 @@ int	key_hook(int keycode, t_data *data)
 
 void	ft_convert_points(t_point **points, t_coord map_size)
 {
+	int	x_aux;
+	int	y_aux;
+	//int	z_aux;
 	int	i;
 	int	j;
 
@@ -40,12 +46,35 @@ void	ft_convert_points(t_point **points, t_coord map_size)
 		j = 0;
 		while (j < map_size.x)
 		{
-			points[i][j].x = cos(M_PI_2 / 3) * points[i][j].x - cos(M_PI_2 / 3) * points[i][j].y + 1220/2;
-			points[i][j].y = ;
-			printf("%d|%d|%d ",points[i][j].x, points[i][j].y, points[i][j].z);
+			x_aux = points[i][j].x + points[i][j].y;
+			y_aux = (points[i][j].x - points[i][j].y) / 2 + points[i][j].z;
+			points[i][j].x = x_aux + 1220 / 2;
+			points[i][j].y = -y_aux + 750 / 2;
+		//printf("%d|%d|%d ",points[i][j].x, points[i][j].y, points[i][j].z);
 			j++;
 		}
-		printf("\n");
+	//	printf("\n");
+		i++;
+	}
+	return ;
+}
+
+void	ft_draw_line(t_point **points, t_data *data, t_coord map_size)
+{
+	(void)points;
+	(void)data;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < map_size.y)
+	{
+		j = 0;
+		while (j < map_size.x)
+		{
+			my_mlx_pixel_put(data, points[i][j].x, points[i][j].y, 0x00FF0000);
+			j++;
+		}
 		i++;
 	}
 	return ;
@@ -60,7 +89,7 @@ int	main(int argc, char *argv[])
 	//int	i = 0;
 
 
-//	atexit(leakss);
+	atexit(leakss);
 	if (argc != 2)
 		return (33);
 	raw_map = NULL;
@@ -71,15 +100,15 @@ int	main(int argc, char *argv[])
 	ft_map_int_array(raw_map, points);
 	ft_map_raw_free(raw_map);
 	ft_convert_points(points, map_size);
-
+	
 	data.mlx_ptr = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx_ptr, 1220, 750, "Wololo!");
 	data.img = mlx_new_image(data.mlx_ptr, 1220, 750);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_lenght, &data.endian);
-	my_mlx_pixel_put(&data, 1220/2, 750/2, 0x00FFFFFF);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel,
+			&data.line_lenght, &data.endian);
+	ft_draw_line(points, &data, map_size);
 	mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, data.img, 0, 0);
 	mlx_key_hook(data.mlx_win, &key_hook, &data);
-	//sleep(5);
 	//system("leaks fdf");
 	mlx_loop(data.mlx_ptr);
 
